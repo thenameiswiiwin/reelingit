8a9155d057049afd1136b83d200b15319729b689
thenameiswiiwin <huyn.nguyen95@gmail.com>
2025-06-20 16:05:17 -0400

    feat(logger): add custom logger with file and stdout output

--- main.go
+++ main.go
@@ -3,14 +3,31 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"github.com/thenameiswiiwin/reelingit/logger"
 )
 
+func initializeLogger() *logger.Logger {
+	logInstance, err := logger.NewLogger("movie.log")
+	if err != nil {
+		log.Fatalf("Failed to initialize logger: %v", err)
+	}
+	defer logInstance.Close()
+	return logInstance
+}
+
 func main() {
+	// Initialize the logger
+	logInstance := initializeLogger()
+
+	// Handle requests to the root path by serving static files from the "public" directory
 	http.Handle("/", http.FileServer(http.Dir("public")))
 
+	// Start the HTTP server on port 8080
 	const address = ":8080"
 	err := http.ListenAndServe(address, nil)
 	if err != nil {
 		log.Fatalf("Failed to start server on %s: %v", address, err)
+		logInstance.Error("Failed to start server", err)
 	}
 }
