ab284c2cb4049ac0d91ad5972f68eca7dead45d0
thenameiswiiwin <huyn.nguyen95@gmail.com>
2025-07-18 13:09:29 -0400

    feat(logger): add custom logging package and integrate into application

--- main.go
+++ main.go
@@ -1,15 +1,32 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/thenameiswiiwin/reelingit/logger"
 )
 
+func initializeLogger() *logger.Logger {
+	logInstance, err := logger.NewLogger("movies-output.log")
+	if err != nil {
+		log.Fatalf("failed to initialize logger: %v", err)
+	}
+	defer logInstance.Close()
+	return logInstance
+}
+
 func main() {
-	http.Handle("/", http.FileServer(http.Dir("public")))
+	logInstance := initializeLogger()
 
 	const addr = ":8080"
+	fmt.Printf("starting server at http://localhost%s\n", addr)
+
+	http.Handle("/", http.FileServer(http.Dir("public")))
+
 	if err := http.ListenAndServe(addr, nil); err != nil {
 		log.Fatalf("failed to start server: %v", err)
+		logInstance.Error("failed to start server", err)
 	}
 }
